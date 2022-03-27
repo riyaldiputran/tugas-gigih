8baf64d87477d2d4be6cdd1ac5f3e4eb9096f45b
riyaldiputran <riyaldiputran007@gmail.com>
2022-03-27 22:17:08 +0800

    Update App.js Memasukkan data kedalam loop (2)

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import './App.css';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import data from './data';
-import All from './components/All';
+import AllData from './components/AllData';
 
 
 
 
 function App() {
-
-  
-  const renderAll = () => data.map((v) => <All key={v.album.images[0].url} img={v.album.images[0].url} album={v.album.name} name={v.album.artists[0].name}/>)
+  const renderAllData = () => data.map((v) => <AllData key={v.album.artists[0].id} img={v.album.images[0].url} album={v.album.name} name={v.album.artists[0].name}/>)
   return (
     <div className="App">
       <header className="App-header">
       <div className="content"> 
-        {renderAll()}
+        {renderAllData()}
         <Button type="button" class="btn btn-secondary btn-outline-light" > <a href={data.uri}></a>Select </Button>
       </div>
       </header>
