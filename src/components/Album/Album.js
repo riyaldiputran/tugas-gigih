26b3f095741c4e57f153a480fec1e7c6285251cb
riyaldiputran <riyaldiputran007@gmail.com>
2022-03-27 22:16:18 +0800

    Update App.js Memasukkan data kedalam loop

--- src/components/Album/Album.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Album = ({album}) => {
-    return <div>
-        <p>{album}</p>
-    </div>
-
-}
-export default Album;
--- src/components/Artists/Artists.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Artists = ({name}) =>{
-    return <div>
-        <p>{name}</p>
-    </div>
-}
-
-export default Artists;
